276b53bfd063f59e8929efe2b07c0a4ae9bb6ad4
github username <github email address>
2019-08-05 11:12:34 -0500

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,8 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
   def create    
@@ -12,9 +14,19 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
-  private
+  	private
 
-  def person_params
-    params.require(:person).permit(:name)
-  end
+    def person_params
+        params.require(:person).permit(
+            :name,
+            addresses_attributes: [
+                :street_address_1,
+                :street_address_2,
+                :city,
+                :state,
+                :zipcode,
+                :address_type
+            ]
+        )
+    end
 end
